f61970ddf35ed7f33cb9faa5110ad6fa376cf710
Chaotic-Potato420 <88218323+Chaotic-Potato420@users.noreply.github.com>
2022-03-06 02:44:04 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function copyToClipboard(text) {
+    var inputc = document.body.appendChild(document.createElement("input"));
+    inputc.value = window.location.href;
+    inputc.focus();
+    inputc.select();
+    document.execCommand('copy');
+    inputc.parentNode.removeChild(inputc);
+    console.log(inputc.value);
+
+    
+    var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+    var toastList = toastElList.map(function(toastEl) {
+      return new bootstrap.Toast(toastEl)
+    })
+    toastList.forEach(toast => toast.show())
+}
